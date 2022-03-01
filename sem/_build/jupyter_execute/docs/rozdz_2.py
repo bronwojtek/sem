0c47752749d7539be3d0485f4009ce7b09d5b6b0
Wojciech Broniowski <wobrogoo1@gmail.com>
2022-03-01 12:24:57 +0100

    start

---
+++ sem/_build/jupyter_execute/docs/rozdz_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Rozdział 2
+
+# In[1]:
+
+
+import numpy as np              # numeric
+import matplotlib.pyplot as plt # plotting
+
+
+# Można chować komórki
+
+# In[2]:
+
+
+def cube(x):
+    return x**3  # cube
+
+
+# In[3]:
+
+
+y=2
+print(y, ' ', cube(y))
+
